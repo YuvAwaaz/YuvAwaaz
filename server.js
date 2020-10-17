850b601a36591af24a256bb144b202596cf538a3
amankumar6 <amankumar786dpsdh@gmail.com>
2020-10-17 19:51:20 +0530

    Revert "feat: restructured"
    This reverts commit d2d0aca1bcf503b1267a1b317a6d0358442f8d9f.

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+var server = require('http').createServer(app)
+app.use(express.json())
+// require('./src/db/mongoose')
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+const port = process.env.PORT || 5000
+
+server.listen(port, () => {
+    console.log(`Server running on port: ${port}`)
+})
